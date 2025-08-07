f8f1121d2e2b949338bf0aa9ef6a82808708851e
Yutsss <adyuta123@gmail.com>
2025-08-08 00:52:09 +0700

    fix: only admin can chance ppn

--- src/routes/PPNRoute.ts
+++ src/routes/PPNRoute.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express';
 import { PPNController } from '../controllers/PPNController';
-import { authMiddleware } from '../middlewares';
+import { authMiddleware, roleMiddleware } from '../middlewares';
+import { Role } from '@prisma/client';
 
 export const ppnRoute: Router = Router();
 
-ppnRoute.get('/', PPNController.getCurrentPPN);
-ppnRoute.patch('/', PPNController.updatePPN);
+ppnRoute.get('/', authMiddleware, PPNController.getCurrentPPN);
+ppnRoute.patch('/', authMiddleware, roleMiddleware([Role.ADMIN]), PPNController.updatePPN);
