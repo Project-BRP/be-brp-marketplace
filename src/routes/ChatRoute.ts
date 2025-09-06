49bd55458b4b4164a1facce6df87481783aac4a2
Yutsss <adyuta123@gmail.com>
2025-09-07 03:51:06 +0700

    chore: hehe

--- src/routes/ChatRoute.ts
+++ src/routes/ChatRoute.ts
@@ -1,7 +1,6 @@
 import { Router } from 'express';
 import { ChatController } from '../controllers/ChatController';
 import { authMiddleware } from '../middlewares';
-import { upload } from '../configs/multer';
 import { roleMiddleware } from '../middlewares';
 import { Role } from '../constants';
 import { uploadArraysMiddleware } from '../middlewares';
